156c887cbf4f47195bb1ebb22bcb056f5b1c6a9e
Serhiy Kucherenko <serhiy.kucherenko@newshore.es>
2020-02-11 23:52:48 +0100

    Initial Commit

---
+++ src/SimpleAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SimpleAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController()
+        {
+        }
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            this._logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var staticWeatherForecast = new WeatherForecast
+            {
+                TemperatureC = -20,
+                Summary = "It makes no difference who we are"
+            };
+
+            var rng = new Random();
+            var forecasts = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            }).ToList();
+
+            forecasts.Add(staticWeatherForecast);
+            return forecasts.ToArray();
+        }
+    }
+}
---
+++ test/SimpleAPI.Test/UnitTest1.cs
@@ -0,0 +1,28 @@
+using System;
+using Xunit;
+using SimpleAPI.Controllers;
+
+namespace SimpleAPI.Test
+{
+    using System.Linq;
+
+    public class UnitTest1
+    {
+        readonly WeatherForecastController controller = new WeatherForecastController();
+
+        [Fact]
+        public void GetReturnsMyName()
+        {
+            var forecasts = this.controller.Get();
+            var lastForecast = forecasts.Last();
+            Assert.Equal("It makes no difference who we are", lastForecast.Summary);
+            Assert.Equal(-20, lastForecast.TemperatureC);
+        }
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
